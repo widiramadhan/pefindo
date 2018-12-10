04c00b0f9310dcedaeefd0e5108dd18acfe3749a
firmandwiputra <firmandwi241990@gmail.com>
2018-12-10 11:01:07 +0700

    savedatacip

---
+++ pages/company/save-data/save-data-cip.php
@@ -0,0 +1,80 @@
+<?php
+if(isset($array['GetCustomReportResponse']['GetCustomReportResult']['aCIP']['bRecordList']['bRecord'])){
+	foreach($array['GetCustomReportResponse']['GetCustomReportResult']['aCIP']['bRecordList']['bRecord'] as $item){
+		/* variable CIP */
+		if($item['bDate']<>NULL){$dateCIP=$item['bDate'];}else{$dateCIP=NULL;}
+		if($item['bGrade']<>NULL){$gradeCIP=$item['bGrade'];}else{$gradeCIP=NULL;}
+		if($item['bProbabilityOfDefault']<>NULL){$probabilityCIP=$item['bProbabilityOfDefault'];}else{$probabilityCIP=NULL;}
+		if($item['bScore']<>NULL){$scoreCIP=$item['bScore'];}else{$scoreCIP=NULL;}
+		if($item['bTrend']<>NULL){$trendCIP=$item['bTrend'];}else{$trendCIP=NULL;}
+		$callCIP = "{call SP_INSERT_CIP_COMPANY(?,?,?,?,?,?)}";
+		$paramsCIP =array(
+					array($pefindoId, SQLSRV_PARAM_IN),
+					array($dateCIP, SQLSRV_PARAM_IN),
+					array($gradeCIP, SQLSRV_PARAM_IN),
+					array($probabilityCIP, SQLSRV_PARAM_IN),
+					array($scoreCIP, SQLSRV_PARAM_IN),
+					array($trendCIP, SQLSRV_PARAM_IN)
+				);
+	$execCIP = sqlsrv_query( $conn, $callCIP, $paramsCIP) or die( print_r( sqlsrv_errors(), true));
+	}else{
+		if(isset($array['GetCustomReportResponse']['GetCustomReportResult']['aCIP']['bRecordList']['bRecord'])){
+			foreach($array['GetCustomReportResponse']['GetCustomReportResult']['aCIP']['bRecordList']['bRecord'] as $item){
+			if($item['bDate']<>NULL){$dateCIP=$item['bDate'];}else{$dateCIP=NULL;}
+		if($item['bGrade']<>NULL){$gradeCIP=$item['bGrade'];}else{$gradeCIP=NULL;}
+		if($item['bProbabilityOfDefault']<>NULL){$probabilityCIP=$item['bProbabilityOfDefault'];}else{$probabilityCIP=NULL;}
+		if($item['bScore']<>NULL){$scoreCIP=$item['bScore'];}else{$scoreCIP=NULL;}
+		if($item['bTrend']<>NULL){$trendCIP=$item['bTrend'];}else{$trendCIP=NULL;}
+		$callCIP = "{call SP_INSERT_CIP_COMPANY(?,?,?,?,?,?)}";
+		$paramsCIP =array(
+					array($pefindoId, SQLSRV_PARAM_IN),
+					array($dateCIP, SQLSRV_PARAM_IN),
+					array($gradeCIP, SQLSRV_PARAM_IN),
+					array($probabilityCIP, SQLSRV_PARAM_IN),
+					array($scoreCIP, SQLSRV_PARAM_IN),
+					array($trendCIP, SQLSRV_PARAM_IN)
+				);
+		$execCIP = sqlsrv_query( $conn, $callCIP, $paramsCIP) or die( print_r( sqlsrv_errors(), true));
+			}
+		}
+	}
+}
+	/* Reason List */
+	$callGetCIP = "{call SP_GET_ID_M_CIP_COMPANY(?)}";
+	$paramsGetCIP = array(array($pefindoId, SQLSRV_PARAM_IN));
+	$execGetCIP = sqlsrv_query( $conn, $callGetCIP, $paramsGetCIP) or die( print_r( sqlsrv_errors(), true));
+	$rowGetCIP = sqlsrv_fetch_array($execGetCIP);
+	$cipId = $rowGetCIP['M_CIP_ID'];
+	
+	if(isset($item['bReasonsList']['bReason'])){
+		foreach($item['bReasonsList']['bReason'] as $data){
+			if($data['bCode']<>NULL){$reasonCodeCIP=$data['bCode'];}else{$reasonCodeCIP=NULL;}
+			if($data['bDescription']<>NULL){$reasonDescCIP=$data['bDescription'];}else{$reasonDescCIP=NULL;}				
+			/* simpan ke table reason list */
+			$callReasonList = "{call SP_INSERT_CIP_REASONS_LIST_COMPANY(?,?,?,?)}";
+			$paramsReasonList = array(
+							array($pefindoId, SQLSRV_PARAM_IN),
+							array($cipId, SQLSRV_PARAM_IN),
+							array($reasonCodeCIP, SQLSRV_PARAM_IN),
+							array($reasonDescCIP, SQLSRV_PARAM_IN)
+						);
+			$execReasonList = sqlsrv_query( $conn, $callReasonList, $paramsReasonList) or die( print_r( sqlsrv_errors(), true));
+	}else{
+		if(isset($item['bReasonsList']['bReason'])){
+			foreach($item['bReasonsList']['bReason'] as $data){
+				if($data['bCode']<>NULL){$reasonCodeCIP=$data['bCode'];}else{$reasonCodeCIP=NULL;}
+				if($data['bDescription']<>NULL){$reasonDescCIP=$data['bDescription'];}else{$reasonDescCIP=NULL;}					
+				/* simpan ke table reason list */
+				$callReasonList = "{call SP_INSERT_CIP_REASONS_LIST_COMPANY(?,?,?,?)}";
+				$paramsReasonList = array(
+								array($pefindoId, SQLSRV_PARAM_IN),
+								array($cipId, SQLSRV_PARAM_IN),
+								array($reasonCodeCIP, SQLSRV_PARAM_IN),
+								array($reasonDescCIP, SQLSRV_PARAM_IN)
+							);
+				$execReasonList = sqlsrv_query( $conn, $callReasonList, $paramsReasonList) or die( print_r( sqlsrv_errors(), true));
+			}
+		}
+	}
+}
+?>
